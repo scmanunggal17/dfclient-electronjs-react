e097a24a14be50764405287fc28d59bebde91c20
CahyaAji <cahya.akk@gmail.com>
2025-06-23 10:53:39 +0700

    chartjs example

--- src/ui/components/DFDetailPlot.jsx
+++ src/ui/components/DFDetailPlot.jsx
@@ -1,3 +1,6 @@
+import { Chart as ChartJS } from "chart.js/auto";
+import { Radar } from "react-chartjs-2";
+
 function DFDetailPlot() {
   return (
     <div
@@ -8,10 +11,32 @@ function DFDetailPlot() {
         height: "100%",
         width: "100%",
         color: "darkred",
-        backgroundColor: "whitegray",
+        backgroundColor: "darkgray",
       }}
     >
-      <h2>Sorry, this page currently under Development!</h2>
+      <Radar
+        data={{
+          labels: ["Speed", "Reliability", "Comfort", "Safety", "Efficiency"],
+          datasets: [
+            {
+              label: "Car A",
+              data: [65, 59, 90, 81, 56],
+              backgroundColor: "rgba(34, 202, 236, 0.2)",
+              borderColor: "rgba(34, 202, 236, 1)",
+              borderWidth: 2,
+              pointBackgroundColor: "rgba(34, 202, 236, 1)",
+            },
+            {
+              label: "Car B",
+              data: [28, 48, 40, 19, 96],
+              backgroundColor: "rgba(255, 99, 132, 0.2)",
+              borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 2,
+              pointBackgroundColor: "rgba(255, 99, 132, 1)",
+            },
+          ],
+        }}
+      />
     </div>
   );
 }
